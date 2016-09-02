5e31e08be648c7d2bfbd75986ca57e2b3d209309
Ravindra Bandara <ravindra.bandara@gmail.com>
2016-09-02 00:55:21 -0400

    couchbase scala view experiments

--- src/main/scala/QuickStart.scala
+++ src/main/scala/QuickStart.scala
@@ -1,6 +1,25 @@
+import com.couchbase.client.java.document.JsonDocument
+import com.couchbase.client.java.view.ViewQuery
+import com.couchbase.spark._
+import org.apache.spark.{SparkConf, SparkContext}
 
 object QuickStart {
   def main(args:Array[String]) = {
-    println("hello spark scala couch")
+
+    // Configure Spark
+    val cfg = new SparkConf()
+      .setAppName("scala-spark-couchbase")
+      .setMaster("local[*]")
+      .set("com.couchbase.bucket.travel-sample", "")
+
+    // Generate The Context
+    val sc = new SparkContext(cfg)
+    // Query by ID
+    sc.couchbaseGet[JsonDocument](Seq("airline_10")).collect().foreach(println)
+
+    // Calling a View
+    println("Query by View")
+    sc.couchbaseView(ViewQuery.from("airline","airline_view").limit(10)).collect().foreach(println)
+
   }
 }
